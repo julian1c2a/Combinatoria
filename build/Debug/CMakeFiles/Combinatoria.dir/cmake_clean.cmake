7a9651df5681f818486d8acaf4b9f849aeb2568e
Julián Calderón Almendros <earendil.elrond.arwen@gmail.com>
2025-10-17 12:30:20 +0200

    Add CMake build files and configuration for Combinatoria project
    - Generated CMake configuration files for the Combinatoria project.
    - Created build scripts and dependency management files for MinGW.
    - Added installation script and compile commands for the project.
    - Configured object file generation and linking for the main source file.
    - Established clean and progress tracking mechanisms in the build process.
    - Included support for installation manifest and local installation options.

---
+++ build/Debug/CMakeFiles/Combinatoria.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Combinatoria.dir/main.cpp.obj"
+  "CMakeFiles/Combinatoria.dir/main.cpp.obj.d"
+  "Combinatoria.exe"
+  "Combinatoria.exe.manifest"
+  "Combinatoria.pdb"
+  "libCombinatoria.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Combinatoria.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
