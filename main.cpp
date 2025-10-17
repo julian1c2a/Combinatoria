508ddbd5b0731d7e07d0ce0d9320b1f1ae044fc3
Julián Calderón Almendros <earendil.elrond.arwen@gmail.com>
2025-10-17 16:56:32 +0200

    Mejorar la precisión de la función factorial al utilizar tipos de datos adecuados y ajustar el formato de retorno

--- main.cpp
+++ main.cpp
@@ -62,33 +62,33 @@ std::ostream& operator<<(std::ostream& os, ulllint_t value) {
 
 constexpr ulllint_t factorial(ulllint_t n) noexcept
 {
-	if      (n  < 2) return 1;
-    else if(n == 2) return 2;
-    else if (n == 3) return 6;
-	else if (n == 4) return 24;
-	else if (n == 5) return 120;
-	else if (n == 6) return 720;
-	else if (n == 7) return 5040;
-	else if (n == 8) return 40320;
-	else if (n == 9) return 362880;
-	else if (n == 10) return 3628800;
-	else if (n == 11) return 39916800;
-	else if (n == 12) return 479001600;
-	else if (n == 13) return 6227020800;
-	else if (n == 14) return 87178291200;
-    else if (n == 15) return 1307674368000;
-    else if (n == 16) return 20922789888000;
-    else if (n == 17) return 355687428096000;
-    else if (n == 18) return 6402373705728000;
-    else if (n == 19) return 121645100408832000;
-    else if (n == 20) return 2432902008176640000;
+    if      (n  < 2) return 1ULL;
+    else if (n == 2) return 2ULL;
+    else if (n == 3) return 6ULL;
+    else if (n == 4) return 24ULL;
+    else if (n == 5) return 120ULL;
+    else if (n == 6) return 720ULL;
+    else if (n == 7) return 5040ULL;
+    else if (n == 8) return 40320ULL;
+    else if (n == 9) return 362880ULL;
+    else if (n == 10) return 3628800ULL;
+    else if (n == 11) return 39916800ULL;
+    else if (n == 12) return 479001600ULL;
+    else if (n == 13) return 6227020800ULL;
+    else if (n == 14) return 87178291200ULL;
+    else if (n == 15) return 1307674368000ULL;
+    else if (n == 16) return 20922789888000ULL;
+    else if (n == 17) return 355687428096000ULL;
+    else if (n == 18) return 6402373705728000ULL;
+    else if (n == 19) return 121645100408832000ULL;
+    else if (n == 20) return 2432902008176640000ULL;
     else if (n < 35) {
-        ulllint_t result = 1;
-        for (ulllint_t i = 2; i <= n; ++i)
+        ulllint_t result = 1ULL;
+        for (ulllint_t i = 2ULL; i <= n; ++i)
             result *= i;
         return result;
     }
-    else return 0; // Placeholder for large n factorial calculation: OVERFLOW
+    else return 0ULL; // Placeholder for large n factorial calculation: OVERFLOW
 }
 
 int main()
